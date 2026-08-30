dfda334fbd9934c661640bda5d499e8a096308da
Zayn Patel <zaynpatelwhs@gmail.com>
2026-08-30 15:45:11 -0700

    Add newtons method for minimization, weak line search algorithm, bfgs method, nonlinear least squares; code and tests

--- src/nonlinear_optimization.py
+++ src/nonlinear_optimization.py
@@ -1,49 +1,138 @@
 """Implements algorithms to solve nonlinear optimization problems
 
 Algorithms include:
-- Newton's method for systems and unconstrained optimization
-- Gradient descent with line search
-- BFGS
+- Newton's method for systems
+- Newton's method for unconstrained minimization
+- Weak line search using a simple backtracking algorithm
+- BFGS method
 - Gauss-Newton method for nonlinear least-squares
 """
 import numpy as np
 import numpy.typing as npt
 
-from scipy.differentiate import jacobian
+from scipy.differentiate import jacobian, hessian
+
+from src.direct_matrix_methods import confirm_column_vector
+from src.iterative_matrix_methods import inner
 
 
-def newtons_method_systems(x: list, p: list, tol=1e-10) -> npt.NDArray:
+def newtons_method_systems(f: function, x: list, p: list):
     """
-    ...    
+    ...
 
-    :param f: Initial vector of functions
-    :type: list
+    :param f: Function that evaluates points
+    :type: function
     :param eval_points: Initial vector of evaluation points
     :type: list
     :param p: Initial vector of direction information
     :type: list
-    :param tol: Accepted error tolerance
-    :type tol: int
     :return: Approximated solution vector
-    :type: ndarray
+    :rtype: list
     """
-    def f(x):
-        x1, x2 = x
-        return [x1**2 - 2*x1 - x2 + 1, x1**2 + x2**2 - 1]
     x_k = x
     p_k = p
-    x_k_plus_1 = None
     for _ in range(50):
         jac_at_x = jacobian(f, x_k)
         result = f(x_k)
         neg_result = [-1 * r for r in result]
         p_k = np.linalg.solve(jac_at_x.df, neg_result)
-        x_k_plus_1 = x_k + p_k
+        x_k = x_k + p_k
+    return x_k
+
+def newtons_method_minimization(phi: function, x_0: list, p: list):
+    """
+    -> You ought to mention that sometimes you will converge to a point but it won't be
+    the minimization point. Instead it might be a saddle point.
+
+    -> You can also mention a relative error termination criteria instead of needing to
+    use the for loop #TODO: I could implement the termination criteria without needing a
+    while loop condition, that might make the code cleaner too since an if check?
+    """
+    x_k = x_0
+    p_k = p
+    for _ in range(50):
+        hes_at_x = hessian(phi, x_k)
+        grad_at_x = jacobian(phi, x_k)  # Jacobian for a single function outputs gradient vector
+        neg_result = [-1 * r for r in grad_at_x.df]
+        p_k = np.linalg.solve(hes_at_x.ddf, neg_result)
+        x_k = x_k + p_k
+    return x_k
+
+def weak_line_search(phi: function, x: npt.NDArray, p: npt.NDArray, cp=1e-4):
+    """
+    Implements a "simple" backtracking algorithm to find an optimal parameter alpha
+
+    -> Explain more about where this comes from, weak strategy is to take decreasing powers of j
+    -> Also, why is this the while condition?
+    """
+    alpha_k = 1  # start the alpha at some "max" value
+    x_k = x
+    while phi(x + (alpha_k * p)) > (phi(x_k) + cp*alpha_k * (inner(jacobian(phi, x_k).df, p))):
+        alpha_k = (alpha_k) * (1/2)
+    return alpha_k
+
+def bfgs_method(phi: function, x_0: npt.NDArray, G_0: npt.NDArray, iter_max=20, grad_err_tol=1e-10):
+    """
+    - Begin with the importance of Quasi-Newton methods for speed and the ability to get to a minimization result
+    but without needing explicit computation of the Hessian matrix (as the Newton methods need)
+
+    - Mention that we update the inverse as opposed to computing a B_k and this is computationally simpler
+    here because we use the Sherman-Morrison-Woodbury formula (this was in the SDSU ppt) which is a rank-two update
+
+    - Explain the derivation and how we get terms like `y_k` and `p_k` *and* that the BFGS method makes the rank-two
+    update using information it found from previous gradients and x values (this is the "incorporation of previous data")
+
+    - Explain that we need a line search method to adjust the direction size we are taking towards the minimum
+    This helps us not overshoot and also adjust the scalar here at each instance. You can also mention why the
+    step size changes at each iteration (we are getting closer to min and overshooting again?)
+
+    - Explain that for SPD B_k we guarantee descent direction because of the inverse energy property
+
+    - Briefly discuss the termination criteria
+    """
+    confirm_column_vector(x_0)
+    n, _ = G_0.shape
+    G_k = G_0
+    x_k = x_0
+    x_k_plus_1 = x_0
+    G_k_plus_1 = G_0
+    for _ in range(iter_max):
+        grad_at_x = jacobian(phi, x_k)
+        if np.linalg.norm(grad_at_x.df) < grad_err_tol:
+            return x_k_plus_1
+        p_k = np.dot(-G_k, grad_at_x.df).reshape(-1, 1)
+        line_search_alpha = weak_line_search(phi, x_k, p_k)
+        x_k_plus_1 = x_k + line_search_alpha*p_k
+        w_k = line_search_alpha*p_k
+        y_k = (jacobian(phi, x_k_plus_1).df - grad_at_x.df).reshape(-1, 1)
+        first_rank_one_update = np.identity(n) - (np.outer(w_k, y_k.T) / inner(y_k, w_k))
+        second_rank_one_update = (np.identity(n) - (np.outer(y_k, w_k.T) / inner(y_k, w_k)))
+        addition_at_end = (np.outer(w_k, w_k.T) / inner(y_k, w_k))
+        G_k_plus_1 = (first_rank_one_update @ G_k @ second_rank_one_update) + addition_at_end
+        x_k = x_k_plus_1
+        G_k = G_k_plus_1
     return x_k_plus_1
 
+def nonlinear_least_squares(phi: function, x_0: npt.NDArray, p_0: npt.NDArray, b: npt.NDArray, iter_max=20, step_size_norm=1e-7):
+    """
+    Docstring for nonlinear_least_squares
 
+    -> Explain why we need to recalculate the Jacobian each time
 
-
-x = [2, 52000, 52000, 520000]
-p = [0, 1, 0, 1]
-print(newtons_method_systems(x, p))
+    [1]: Reference on gradient of the min phi(x):
+    https://math.stackexchange.com/questions/3508373/taking-the-gradient-of-f-mathbfx-frac12-mathbfa-mathbfx-ma
+    """
+    x_k = x_0
+    p_k = p_0
+    for _ in range(iter_max):
+        jac_at_x = jacobian(phi, x_k)
+        jacobian_T = jac_at_x.df.T
+        J = jacobian_T @ jac_at_x.df
+        residual = b - phi(x_k)
+        j = jacobian_T @ residual
+        p_k = np.linalg.solve(J, j)
+        norm = np.linalg.norm(p_k)
+        if norm < step_size_norm:
+            return x_k
+        x_k = x_k + p_k
+    return x_k
---
+++ tests/test_nonlinear_optimization.py
@@ -0,0 +1,64 @@
+import numpy as np
+from scipy.optimize import least_squares, minimize
+
+from src import nonlinear_optimization as nlo
+
+def test_newtons_method_systems_first_root():
+    # Benchmark testing with a known example from the textbook (Example 9.2)
+    def f(x):
+        x1, x2 = x
+        return [x1**2 - 2*x1 - x2 + 1, x1**2 + x2**2 - 1]
+    x = [1, 1]  # The initial guess matters more than p
+    p = [-2, 1]
+    r = nlo.newtons_method_systems(f, x, p)
+    assert np.allclose(r, [1, 0])
+
+def test_newtons_method_systems_second_root():
+    # Benchmark testing with a known example from the textbook (Example 9.2)
+    def f(x):
+        x1, x2 = x
+        return [x1**2 - 2*x1 - x2 + 1, x1**2 + x2**2 - 1]
+    x = [-1, 1]
+    p = [1, 1]
+    r = nlo.newtons_method_systems(f, x, p)
+    assert np.allclose(r, [0, 1])
+
+def test_newtons_method_minimization():
+    # Benchmark testing with known function from textbook (Example 9.5)
+    def phi(x):
+        x1, x2 = x
+        return 1/2*((1.5 - x1*(1-x2))**2 + (2.25 - x1*(1-x2**2))**2 + (2.625 - x1*(1-x2**3))**2)
+    x_0 = [8, 0.2]
+    p = [1, 1]
+    r = nlo.newtons_method_minimization(phi, x_0, p)
+    assert np.allclose(r, [3, .5], rtol=1e-3)
+
+def test_newtons_method_minimization_saddle_point():
+    # Benchmark testing with known function from textbook (Example 9.5)
+    def phi(x):
+        x1, x2 = x
+        return 1/2*((1.5 - x1*(1-x2))**2 + (2.25 - x1*(1-x2**2))**2 + (2.625 - x1*(1-x2**3))**2)
+    x_0 = [8, 0.8]
+    p = [1, 1]
+    r = nlo.newtons_method_minimization(phi, x_0, p)
+    assert np.allclose(r, [0, 1], rtol=1e-3)
+
+def test_bfgs_method():
+    def phi(x):
+        x1, x2 = x
+        return 1/2*((1.5 - x1*(1-x2))**2 + (2.25 - x1*(1-x2**2))**2 + (2.625 - x1*(1-x2**3))**2)
+    x_0 = np.array([[1.3], [0.7]], dtype=float)
+    sp_x_0 = np.array([1.3, 0.7])  # scipy only accepts 1D array
+    res = minimize(phi, sp_x_0, method='BFGS', tol=1e-6)
+    r = nlo.bfgs_method(phi, x_0, np.identity(len(x_0)))  # Outputs 1D array
+    assert np.allclose(r, res.x.reshape(-1, 1))  # Reshape so arrays are the same size for comparison
+
+def test_nonlinear_least_squares():
+    def phi(x):
+        return [(np.sin(x[0]*x[1]) + 3*x[2]), (np.cos(x[0]-x[1]) * 2*x[1]**2), (np.exp(x[0]+2*x[1]) + x[2])]
+    x0 = np.array([3.5, 0.8, 7], dtype=float)
+    p_0 = np.array([1, 1, 1], dtype=float)
+    b = np.array([2, -2, 8], dtype=float)
+    r = nlo.nonlinear_least_squares(phi, x0, p_0, b)
+    norm_resi = np.linalg.norm(b - phi(r))  # check the residual
+    assert np.allclose(norm_resi, np.float64(0.0))
